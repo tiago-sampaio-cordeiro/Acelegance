74eee37a4e4af0c9889b85873186974e4858d06f
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2025-05-10 01:17:55 -0300

    first commit

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acelegance</title>
+    <link rel="stylesheet" href="/assets/css/application.css">
+
+</head>
+
+<body>
+    <header>
+        <h1>Acelegance</h1>
+    </header>
+
+    <p>Uma aplicação para revendedores de joias</p>
+    
+</body>
+</html>
