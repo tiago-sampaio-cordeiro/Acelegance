afe8ef49f0a00495827506c0578f2dbde4f1d350
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2025-05-23 15:39:55 -0300

    refatorando create.php para OO

--- public/pages/products/create.php
+++ public/pages/products/create.php
@@ -1,5 +1,7 @@
 <?php
 
+require '/var/www/app/models/Product.php';
+
 $method = $_SERVER['REQUEST_METHOD'];
 
 if ($method !== 'POST') {
@@ -7,21 +9,34 @@ if ($method !== 'POST') {
     exit;
 }
 
+$params = $_POST['product'];
 
-$product = $_POST['product'];
-$name = trim($product['name']);
 
-$errors = [];
-if (empty($name)) {
-    $errors['name'] = 'O campo nome do produto não pode ser vazio.';
-}
+$product = new Product(name: $params['name']);
 
-if (empty($errors)) {
-    define('DB_PATH', '/var/www/database/products.txt');
-    file_put_contents(DB_PATH, $name . PHP_EOL, FILE_APPEND);
+if ($product->save()) {
     header('Location: /pages/products');
 } else {
     $title = 'Cadastro de Produtos';
     $view = '/var/www/app/views/products/new.phtml';
     require '/var/www/app/views/layouts/application.phtml';
 }
+
+
+// $product = $_POST['product'];
+// $name = trim($product['name']);
+
+// $errors = [];
+// if (empty($name)) {
+//     $errors['name'] = 'O campo nome do produto não pode ser vazio.';
+// }
+
+// if (empty($errors)) {
+//     define('DB_PATH', '/var/www/database/products.txt');
+//     file_put_contents(DB_PATH, $name . PHP_EOL, FILE_APPEND);
+//     header('Location: /pages/products');
+// } else {
+//     $title = 'Cadastro de Produtos';
+//     $view = '/var/www/app/views/products/new.phtml';
+//     require '/var/www/app/views/layouts/application.phtml';
+// }
