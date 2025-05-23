954a0674102565e8d6d3933620c797c7d6f169f5
Tiago Sampaio Cordeiro <145937944+tiago-sampaio-cordeiro@users.noreply.github.com>
2025-05-23 04:36:21 -0300

    implementado retrieve para os produtos (#18)

--- public/pages/products/show.php
+++ public/pages/products/show.php
@@ -1,11 +1,15 @@
 <?php
 
+$id = intval($_GET['id']);
+
 define('DB_PATH', '/var/www/database/products.txt');
 
 $products = file(DB_PATH, FILE_IGNORE_NEW_LINES);
 
-$title = "Produtos";
+$product['name'] = $products[$id];
 
-$view = '/var/www/app/views/products/index.phtml';
+$title = "Detalhes do Produto #{$id}";
+
+$view = '/var/www/app/views/products/show.phtml';
 
 require '/var/www/app/views/layouts/application.phtml';
