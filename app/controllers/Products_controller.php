9f35c7fcd43a64cc72ba08ec3699b7c7a02578b6
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2025-05-23 23:12:19 -0300

    refatorando delete para OO

---
+++ app/controllers/Products_controller.php
@@ -0,0 +1,130 @@
+<?php
+require '/var/www/app/models/Product.php';
+
+class Products_controller
+{
+
+    private string $layout = 'application';
+
+    public function index()
+    {
+
+        $products = Product::all();
+
+        $title = "Produtos";
+
+        $this->render('index', compact('products', 'title'));
+    }
+
+    public function show()
+    {
+        $id = intval($_GET['id']);
+
+        $product = Product::findById($id);
+
+        $title = "Detalhes do Produto #{$id}";
+
+        $this->render('show', compact('product', 'title'));
+    }
+
+
+    public function new()
+    {
+        $title = 'Cadastro de Produtos';
+
+        $view = '/var/www/app/views/products/new.phtml';
+
+        $product = new Product();
+
+        $this->render('new', compact('product', 'title'));
+    }
+
+    public function create()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if ($method !== 'POST') {
+            $this->redirectTo('/pages/products');
+        }
+
+        $params = $_POST['product'];
+        $product = new Product(name: $params['name']);
+
+        if ($product->save()) {
+            $this->redirectTo('/pages/products');
+        } else {
+            $title = 'Cadastro de Produtos';
+            $this->render('new', compact('product', 'title'));
+        }
+    }
+
+    public function edit()
+    {
+        $id = intval($_GET['id']);
+
+        $product = Product::findById($id);
+
+        $title = "Edição do Produto #{$id}";
+
+        $this->render('edit', compact('product', 'title'));
+    }
+
+    public function update()
+    {
+
+        $method = $_REQUEST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
+        if ($method !== 'PUT') {
+            $this->redirectTo('/pages/products');
+        }
+
+
+        $params = $_POST['product'];
+
+        $product = Product::findById($params['id']);
+        $product->setName($params['name']);
+
+        if ($product->save()) {
+            $this->redirectTo('/pages/products');
+        } else {
+            if ($product->save()) {
+                header('Location: /pages/products');
+            } else {
+                $title = "Atualizar produto #{$product->getId()}";
+                $this->render('edit', compact('product', 'title'));
+            }
+        }
+    }
+
+    public function destroy()
+    {
+        $method = $_REQUEST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
+        if ($method !== 'DELETE') {
+            $this->redirectTo('/pages/products');
+        }
+
+        $param = $_POST['product'];
+        $product = Product::findById($param['id']);
+
+        if ($product) {
+            $product->delete();
+            $this->redirectTo('/pages/products');
+        }
+    }
+
+
+    private function render($view, $data = [])
+    {
+
+        extract($data);
+        $view = '/var/www/app/views/products/' . $view . '.phtml';
+        require '/var/www/app/views/layouts/application.phtml';
+    }
+
+    private function redirectTo($location)
+    {
+        header('Location: ' . $location);
+        exit;
+    }
+}
--- public/pages/products/update.php
+++ public/pages/products/update.php
@@ -1,32 +1,5 @@
 <?php
 
-require '/var/www/app/models/Product.php';
-
-$method = $_REQUEST['_method'] ?? $_SERVER['REQUEST_METHOD'];
-
-if ($method !== 'PUT') {
-    header('Location: /pages/products');
-    exit;
-}
-
-
-$product = $_POST['product'];
-
-$id = $product['id'];
-$name = trim($product['name']);
-
-$product = Product::findById($id);
-$product->setName($name);
-
-if ($product->save()) {
-    header('Location: /pages/products');
-    exit;
-} else {
-    if ($product->save()) {
-        header('Location: /pages/products');
-    } else {
-        $title = "Atualizar produto #{$id}";
-        $view = '/var/www/app/views/products/edit.phtml';
-        require '/var/www/app/views/layouts/application.phtml';
-    }
-}
+require '/var/www/app/controllers/Products_controller.php';
+$controller = new Products_controller();
+$controller->update();
