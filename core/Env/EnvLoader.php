bc66d46ba75157ce3cf539643a2f78664b4a490c
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2025-05-25 01:42:03 -0300

    refatorado pasta Env

---
+++ core/Env/EnvLoader.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Core\Env;
+
+use Core\Constants\Constants;
+
+class EnvLoader
+{
+    public static function init()
+    {
+        $envs = parse_ini_file(Constants::rootPath()->join('.env'));
+
+        foreach ($envs as $key => $value) {
+            if (!isset($_ENV[$key])) {
+                $_ENV[$key] = $value;
+            }
+        }
+    }
+}
