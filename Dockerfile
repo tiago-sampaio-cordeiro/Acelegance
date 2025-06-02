1c6fdbbe419718df73072acde1bb57de4628fe4c
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2025-06-02 01:20:40 -0300

    setup PDO and PDO driver also database sql and command to reset

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM php:8.3.4-fpm
+
+RUN docker-php-ext-install pdo pdo_mysql
+RUN docker-php-ext-enable pdo_mysql
+
---
+++ database/schema.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
